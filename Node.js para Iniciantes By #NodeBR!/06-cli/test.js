7003c4547d3c034ee41592ee83cfb16a9926a6b5
Renato Siqueira <renatoelysiqueira@gmail.com>
2018-12-16 01:50:32 -0300

    43%

--- Node.js para Iniciantes By #NodeBR!/06-cli/test.js
+++ Node.js para Iniciantes By #NodeBR!/06-cli/test.js
@@ -21,6 +21,11 @@ describe('Suíte de Manipulação de Herois', () => {
         const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
         deepEqual(actual, expected)
     })
+    it('Deve remover um heroi por id', async () => {
+        const expected = true
+        const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id)
+        deepEqual(resultado, expected)
+    })
     it('Deve atualizar um heroi pelo id', async () => {
         const expected ={
             ...DEFAULT_ITEM_ATUALIZAR,
