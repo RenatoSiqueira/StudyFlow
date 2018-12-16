833046b47f8667580dd11d3cb33ef77b1eb1c3e6
Renato Siqueira <renatoelysiqueira@gmail.com>
2018-12-16 01:34:23 -0300

    42%

--- Node.js para Iniciantes By #NodeBR!/06-cli/test.js
+++ Node.js para Iniciantes By #NodeBR!/06-cli/test.js
@@ -2,10 +2,12 @@ const { deepEqual, ok } = require('assert')
 const database = require('./database')
 
 const DEFAULT_ITEM_CADASTRAR = { nome: 'Flash', poder: 'Speed', id: 1 }
+const DEFAULT_ITEM_ATUALIZAR = { nome : 'Lanterna Verde', poder: 'Energia do Anel', id: 2 }
 
 describe('Suíte de Manipulação de Herois', () => {
     before(async () => {
         await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
+        await database.cadastrar(DEFAULT_ITEM_ATUALIZAR)
     })
     it('Deve pesquisar um heroi, usando arquivos', async () => {
         const expected = DEFAULT_ITEM_CADASTRAR
@@ -18,5 +20,19 @@ describe('Suíte de Manipulação de Herois', () => {
         const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR)
         const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id)
         deepEqual(actual, expected)
+    })
+    it('Deve atualizar um heroi pelo id', async () => {
+        const expected ={
+            ...DEFAULT_ITEM_ATUALIZAR,
+            nome: 'Batman',
+            poder: 'Dinheiro'
+        }
+        const novoDado = {
+            nome: 'Batman',
+            poder: 'Dinheiro'
+        }
+        await database.atualizar(DEFAULT_ITEM_ATUALIZAR.id, novoDado)
+        const [resultado] = await database.listar(DEFAULT_ITEM_ATUALIZAR.id)
+        deepEqual(resultado, expected)
     })
 })
